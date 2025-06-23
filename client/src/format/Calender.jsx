2e50ce1e482b3228542c4f3128600520efcdc8e8
Naman Jain <naman.jain@antrepriz.com>
2025-06-23 17:58:53 +0530

    fix for date input

--- client/src/format/Calender.jsx
+++ client/src/format/Calender.jsx
@@ -8,15 +8,16 @@
       return (
         <div className='w-full'>
           <p className="text-sm">Date</p>
-          <DatePicker
-            
-            selected={date}
-            onChange={(date) => setDate(date)}
-            dateFormat="MM/dd/yyyy" // Customize date format
-            
-            placeholderText="dd/mm/yyyy"
-            className='text-sm border-2 border-gray-300 bg-gray-100 w-full mt-1 h-8 rounded p-2'
-          />
+          <div className="w-full">
+            <DatePicker
+              selected={date}
+              onChange={(date) => setDate(date)}
+              dateFormat="MM/dd/yyyy" // Customize date format
+              placeholderText="dd/mm/yyyy"
+              className='text-sm border-2 border-gray-300 bg-gray-100 w-full mt-1 h-8 rounded p-2'
+              wrapperClassName="w-full"
+            />
+          </div>
         </div>
       );
     }
